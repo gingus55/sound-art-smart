bc7d09114f3bb7be828a3296c4d284f81c02a6c4
jj77847 <81172135+jj77847@users.noreply.github.com>
2021-10-23 16:20:34 +0100

    deleted files duplicated

--- assets/js/jambotmockup.js
+++
@@ -1,7 +0,0 @@
-$(function () {
-  var includes = $("[data-include]");
-  $.each(includes, function () {
-    var file = "views/" + $(this).data("include") + ".html";
-    $(this).load(file);
-  });
-});
