41acc116d2f6dd4024cdd46a04e8eb5af75e647e
gingus55 <gingus55@hotmail.com>
2021-10-30 15:43:27 +0100

    boiller plate in place for favourites

---
+++ assets/js/favourites.js
@@ -0,0 +1,14 @@
+console.log("howdy planet");
+
+// NAVBAR -- mobile menu
+
+const navbarMenu = function () {
+  const burgerIcon = $("#burger");
+  const navbarMenu = $("#nav-links");
+
+  const isActiveBurgerIcon = function () {
+    navbarMenu.toggle("is-active");
+  };
+
+  burgerIcon.on("click", isActiveBurgerIcon);
+};
