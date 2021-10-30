9c50784bab97d0fe9d32ccd468e6147fe290a7b6
gingus55 <gingus55@hotmail.com>
2021-10-30 15:59:13 +0100

    favourites ready to receive data

--- assets/js/favourites.js
+++ assets/js/favourites.js
@@ -13,9 +13,50 @@ const navbarMenu = function () {
   burgerIcon.on("click", isActiveBurgerIcon);
 };
 
+function InitializeLocalStorage() {
+  const objectInfo = JSON.parse(localStorage.getItem("object-data"));
+  if (!objectInfo) {
+    return localStorage.setItem("object-data", JSON.stringify([]));
+  }
+}
+
+function getFromLocalStorage() {
+  const localStorageData = JSON.parse(localStorage.getItem("object-data"));
+  return localStorageData === null ? JSON.stringify([]) : localStorageData;
+}
+
+const favouriteContainer = $("#favourite-container");
+
+const renderFavouriteCards = function (lsdata) {
+  favouriteContainer.empty();
+  const constructFavouriteCards = function (each) {
+    const redirectLink = `./view-object-results.html?objectid=${each.objectID}`;
+    return `<div id="object-id-card-container" class="object-card animate__animated animate__zoomIn m-5">
+      <div class="card-img">
+        <img src="${each.imageUrl}" alt="" />
+      </div>
+      <div class="card-header-title has-text-centered is-size-5 is-italic">
+        ${each.title}
+      </div>
+      <div class="view-btn-container">
+        <a href=${redirectLink} class="view-object-btn button" id="${JSON.stringify(
+      each.objectID
+    )}">View More Info</a>
+    </div>`;
+  };
+  const favouriteCards = lsdata.map(constructFavouriteCards).join("");
+  const favouriteCardContainer = `<section id="object-cards-container">${favouriteCards}</section>`;
+
+  favouriteContainer.append(favouriteCardContainer);
+};
+
 const onReady = function () {
   // call mobile navbar burger menu
   navbarMenu();
+
+  const lsdata = getFromLocalStorage();
+
+  renderFavouriteCards(lsdata);
 };
 
 $(document).ready(onReady);
