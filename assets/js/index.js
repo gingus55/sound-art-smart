8ca450671f95ccd74cf95ed3eb47ff572c7fdf5b
Kayle Rieger Patton <kayle.patton22@gmail.com>
2021-10-31 15:59:23 +0000

    get and transform quote data

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,11 +1,40 @@
-// Mobile burger menu
 const burgerMenu = $("#burger");
 const navbarMenu = $("#nav-links");
+// target quote container
+const quoteContainer = $("#landing-quote-container");
 
+// set API URL
+const quoteOfTheDayUrl = "https://quotes.rest/qod?category=art&language=en";
+
+// Mobile burger menu
 const activateBurgerMenu = function () {
   navbarMenu.toggle("is-active");
 };
 
 $(burgerMenu).click(activateBurgerMenu);
 
-// Quote of the day
+// transform quote data
+const getQuoteOfTheDayContents = function (quoteOfTheDayResult) {
+  const quoteOfTheDayContents = quoteOfTheDayResult.contents.quotes[0];
+
+  return {
+    quote: quoteOfTheDayContents.quote,
+    author: quoteOfTheDayContents.author,
+  };
+};
+
+// Quote of the day API call
+const getQuoteOfTheDayData = async function () {
+  const quoteOfTheDayResponse = await fetch(quoteOfTheDayUrl);
+  const quoteOfTheDayResult = await quoteOfTheDayResponse.json();
+
+  const quoteOfTheDayData = getQuoteOfTheDayContents(quoteOfTheDayResult);
+
+  // console.log(quoteOfTheDayResult);
+};
+
+const onReady = function () {
+  getQuoteOfTheDayData();
+};
+
+$(document).ready(onReady);
