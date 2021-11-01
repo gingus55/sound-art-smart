5f97c6dda483a172e6b7d5cb0ee1669a2d52be0a
Dahir Mohamed <dmoabdi98@gmail.com>
2021-11-01 22:28:04 +0000

    api data transformation function

--- assets/js/view-object-results.js
+++ assets/js/view-object-results.js
@@ -11,97 +11,117 @@ const navbarMenu = function () {
   burgerIcon.on("click", isActiveBurgerIcon);
 };
 
-console.log(keyWords);
-
 // store container of the results page in a variable to append into
 const bodyContainer = $("body");
 
-const handleResponse = function (response) {
-  return response.json();
-};
+const getObjectData = function (dataInfo) {
+  const objectData = {
+    title: dataInfo.title,
+    accessionYear: dataInfo.accessionYear,
+    artistName: dataInfo.artistDisplayName,
+    artistBio: dataInfo.artistDisplayBio,
+    class: dataInfo.classification,
+    line: dataInfo.creditLine,
+    department: dataInfo.department,
+    size: dataInfo.dimensions,
+    medium: dataInfo.medium,
+    date: dataInfo.objectDate,
+    name: dataInfo.objectName,
+    image: dataInfo.primaryImage,
+  };
 
-// Make Api call
-const makeObjectCall = async function () {
-  const urlObjectId = new URL(window.location).search.split("=")[1];
-  const url = `https://collectionapi.metmuseum.org/public/collection/v1/objects/${urlObjectId}`;
-  const data = await apiRequest(url);
-  console.log(data);
-  // fetch(url).then(handleResponse).then(handleData);
+  return objectData;
 };
 
 // construct the main page of the individual results page
 const renderMainSectionOfResults = function (objectData) {
   const mainResultsSectionHtml = `
+  <div class="is-size-1 title has-text-centered mt-5 mb-5">ART WORK NAME</div>
+
+  <div class="btn-container has-text-centered mb-5">
+    <button class="button is-medium is-danger">Save</button>
+  </div>
+
   <main class="margin-adjustment">
-    <!-- Art work image and art work facts column    -->
+      <!-- Art work image and art work facts column    -->
 
-    <div class="is-flex" id="img-facts-container">
-      <!-- pAINTING CARD -->
-      <div class="paint-card m-5">
-        <div class="card-img">
-          <img
-            src="https://dynaimage.cdn.cnn.com/cnn/q_auto,w_412,c_fill,g_auto,h_412,ar_1:1/http%3A%2F%2Fcdn.cnn.com%2Fcnnnext%2Fdam%2Fassets%2F190430171751-mona-lisa.jpg”
-         alt=“landscape-painting"
-            alt=""
-          />
-        </div>
-      </div>
-
-      <!--ART WORK FACTS LIST -->
-      <div class="art-works-fact-container">
-        <div class="title has-text-centered is-underlined mb-6">
-          Facts About The Art Work
-        </div>
-        <div
-          class="
-            is-flex
-            is-flex-direction-row
-            is-flex-wrap-wrap
-            is-align-content-center
-          "
-          id="facts"
-        >
-          <div class="is-size-6 pl-5">
-            <ul>
-              <li>
-                <span class="has-text-weight-bold">Title:</span> Wheat Field
-                with Cypresses
-              </li>
-              <li>
-                <span class="has-text-weight-bold">Artist:</span> Vincent van
-                Gogh (Dutch, Zundert 1853–1890 Auvers-sur-Oise)
-              </li>
-              <li><span class="has-text-weight-bold">Date:</span> 1889</li>
-              <li>
-                <span class="has-text-weight-bold">Medium:</span> Oil on
-                canvas
-              </li>
-            </ul>
-          </div>
-          <div class="is-size-6 pl-5">
-            <ul>
-              <li>
-                <span class="has-text-weight-bold">Dimensions:</span> 28 7/8 ×
-                36 3/4 in. (73.2 × 93.4 cm)
-              </li>
-              <li>
-                <span class="has-text-weight-bold">Classification:</span>
-                Paintings
-              </li>
-              <li>
-                <span class="has-text-weight-bold">Credit Line:</span>
-                Purchase, The Annenberg Foundation Gift, 1993
-              </li>
-              <li>
-                <span class="has-text-weight-bold">Accession Number:</span>
-                1993.132
-              </li>
-            </ul>
+      <div class="is-flex" id="img-facts-container">
+        <!-- pAINTING CARD -->
+        <div class="paint-card m-5">
+          <div class="card-img">
+            <img
+              src="https://dynaimage.cdn.cnn.com/cnn/q_auto,w_412,c_fill,g_auto,h_412,ar_1:1/http%3A%2F%2Fcdn.cnn.com%2Fcnnnext%2Fdam%2Fassets%2F190430171751-mona-lisa.jpg”
+        alt=“landscape-painting"
+              alt=""
+            />
           </div>
         </div>
+
+        <!--ART WORK FACTS LIST -->
+        <div class="art-works-fact-container">
+          <div class="title has-text-centered is-underlined mb-6">
+            Facts About The Art Work
+          </div>
+          <div
+            class="
+              is-flex
+              is-flex-direction-row
+              is-flex-wrap-wrap
+              is-align-content-center
+            "
+            id="facts"
+          >
+            <div class="is-size-6 pl-5">
+              <ul>
+                <li>
+                  <span class="has-text-weight-bold">Object Name:</span> Wheat
+                  Field with Cypresses
+                </li>
+
+                <li>
+                  <span class="has-text-weight-bold">Artist:</span> Vincent van
+                  Gogh (Dutch, Zundert 1853–1890 Auvers-sur-Oise)
+                </li>
+
+                <li><span class="has-text-weight-bold">Date:</span> 1889</li>
+
+                <li>
+                  <span class="has-text-weight-bold">Medium:</span> Oil on
+                  canvas
+                </li>
+
+                <li>
+                  <span class="has-text-weight-bold">Department:</span> Oil on
+                  canvas
+                </li>
+              </ul>
+            </div>
+            <div class="is-size-6 pl-5">
+              <ul>
+                <li>
+                  <span class="has-text-weight-bold">Dimensions:</span> 28 7/8 ×
+                  36 3/4 in. (73.2 × 93.4 cm)
+                </li>
+                <li>
+                  <span class="has-text-weight-bold">Classification:</span>
+                  Paintings
+                </li>
+                <li>
+                  <span class="has-text-weight-bold">Credit Line:</span>
+                  Purchase, The Annenberg Foundation Gift, 1993
+                </li>
+                <li>
+                  <span class="has-text-weight-bold">Accession Number:</span>
+                  1993.132
+                </li>
+              </ul>
+            </div>
+          </div>
+        </div>
       </div>
-    </div>
-  </main>`;
+    </main>
+
+  `;
 
   return mainResultsSectionHtml;
 };
@@ -285,6 +305,23 @@ const renderAllAccordions = function (artWorkAccordion, userArray) {
   renderConstantAccordion(artWorkAccordion);
   generateUserAccordion(userArray);
   handleAccordionFunctionality();
+};
+
+const handleResponse = function (response) {
+  return response.json();
+};
+
+// Make Api call
+const makeObjectCall = async function () {
+  const urlObjectId = new URL(window.location).search.split("=")[1];
+  const url = `https://collectionapi.metmuseum.org/public/collection/v1/objects/${urlObjectId}`;
+
+  const data = await apiRequest(url);
+
+  console.log(data);
+
+  return data;
+  // fetch(url).then(handleResponse).then(handleData);
 };
 
 // document on load
