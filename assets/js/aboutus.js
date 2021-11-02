410502e404a71d6981b81637e6e2696cbab19b62
Elizabeth Kotwicka <elizabeth.kotwicka@hotmail.co.uk>
2021-11-02 20:04:49 +0000

    adding some functionality to the about us page

--- assets/js/aboutus.js
+++ assets/js/aboutus.js
@@ -1,2 +1,11 @@
-console.log("hello");
 
+
+const aboutUsContent = [
+    { name: "Chris", status: "status", bio: "bio" },
+    { name: "Kayle", status: "status", bio: "bio" },
+    { name: "Jonathan", status: "status", bio: "bio" },
+    { name: "Elizabeth", status: "status", bio: "bio" },
+    { name: "Dahir", status: "status", bio: "bio" },
+  ];
+
+if 
