2d5addf868815e70ab858b47dac892d33591a0ca
Dahir Mohamed <dmoabdi98@gmail.com>
2021-11-02 18:49:33 +0000

    wip

--- assets/js/view-object-results.js
+++ assets/js/view-object-results.js
@@ -95,13 +95,15 @@ const renderMainSectionOfResults = function (objectData) {
                   <span class="has-text-weight-bold">Medium:</span> ${objectData.medium}
                 </li>
 
-                <li>
-                  <span class="has-text-weight-bold">Department:</span> ${objectData.department}
-                </li>
               </ul>
             </div>
             <div class="is-size-6 pl-5">
               <ul>
+              
+              <li>
+              <span class="has-text-weight-bold">Department:</span> ${objectData.department}
+            </li>
+
                 <li>
                   <span class="has-text-weight-bold">Dimensions:</span> ${objectData.size}
                 </li>
@@ -202,32 +204,36 @@ const accordionContent = [
     info: "Animals have been a source of inspiration for many artists. From art about rural life and growth, to myths and legends, animals are used in art in many different ways. Art can help us explore our relationship to wildlife and can help us think about how we care for animals and the environment. When it comes to animals, exploring habitats is one of the central themes for artists. With a world of textures, colours and shapes to explore, nature is an exciting feature in many artworks. Animals in artworks can make us consider our own relationship to nature. How do we treat wildlife and its environment? Are there some animals we look after more than others?",
   },
   {
+    title: "People",
+    info: ".......................",
+  },
+  {
     title: "Oil",
-    info: `abstract painting are ..........`,
+    info: "Oil paints were first used between the 5th & 9th century to decorate shields and other objects in western Afghanistan.The use of oil paints did not appear in Europe until the 15th century. Prior to this, most artist used egg tempera. Prior to the 19th Century, an artist's studio looked like a laboratory. The artists had to grind the pigments, boil the oil and use exact formulas to create their oil paints. Apprentices were hired to help with this chore.",
   },
   {
     title: "Watercolour",
-    info: `places painting are ..........`,
+    info: "Watercolors tend to be transparent when applied to your painting surface. that means it allows the light to pass through your painting canvas or painting paper. You can purchase watercolor that comes in tubes or in pans.  Watercolors in tubes are easy to use as you can directly apply them to your painting surface. You can also dilute tube watercolors with water to achieve your desired color or tint. On the other hand, watercolors in pans usually come with palette in them. Watercolors in pans are more expensive than tube watercolors. The most commonly used watercolors by artists are the ones in tubes.",
   },
   {
     title: "Tempera",
-    info: `places painting are ..........`,
+    info: "tempera painting, painting executed with pigment ground in a water-miscible medium. The word tempera originally came from the verb temper, “to bring to a desired consistency.” Dry pigments are made usable by “tempering” them with a binding and adhesive vehicle.Tempera is an ancient medium, having been in constant use in most of the world’s cultures until it was gradually superseded by oil paints in Europe, during the Renaissance. Tempera was the original mural medium in the ancient dynasties of Egypt, Babylonia, Mycenaean Greece, and China and was used to decorate the early Christian catacombs. It was employed on a variety of supports, from the stone stelae (or commemorative pillars), mummy cases, and papyrus rolls of ancient Egypt to the wood panels of Byzantine icons and altarpieces and the vellum leaves of medieval illuminated manuscripts. ",
   },
   {
     title: "Egyptian Art",
-    info: `places painting are ..........`,
+    info: "Much of the artwork created by the Ancient Egyptians had to do with their religion. They would fill the tombs of the Pharaohs with paintings and sculptures. Much of this artwork was there to help the Pharaohs in the afterlife. Temples were another popular place for art. The temples often held large statues of their gods as well as many paintings on the walls. The Egyptians are famous for their giant works of sculptures. Some examples of this include the Great Sphinx of Giza and the statues of Ramses II at the Abu Simbel temples. Although they are famous for their giant statues, the Egyptians also carved smaller, more ornate sculptures. They used various materials including alabaster, ivory, limestone, basalt, wood gilded with gold, and sometimes even solid gold. In Ancient Egypt the tomb walls of the rich and powerful were often filled with paintings. These paintings were there to help the person in the afterlife. They often depicted the person buried passing into the afterlife. They would show scenes of this person happy in the afterlife. In one painting the man buried is shown hunting and his wife and son are in the picture.",
   },
   {
     title: "Greek and Roman Art",
-    info: `places painting are ..........`,
+    info: "The art of the ancient Greeks and Romans is called classical art. This name is used also to describe later periods in which artists looked for their inspiration to this ancient style. The Romans learned sculpture and painting largely from the Greeks and helped to transmit Greek art to later ages. Classical art owes its lasting influence to its simplicity and reasonableness, its humanity, and its sheer beauty. The first and greatest period of classical art began in Greece about the middle of the 5th century bc. By that time Greek sculptors had solved many of the problems that faced artists in the early archaic period. They had learned to represent the human form naturally and easily, in action or at rest. They were interested chiefly in portraying gods, however. They thought of their gods as people, but grander and more beautiful than any human being. They tried, therefore, to portray ideal beauty rather than any particular person. Their best sculptures achieved almost godlike perfection in their calm, ordered beauty.",
   },
   {
     title: "Islamic Art",
-    info: `places painting are ..........`,
+    info: "Islamic art encompasses the visual arts produced from the seventh century onward by both Muslims and non-Muslims who lived within the territory that was inhabited by, or ruled by, culturally Islamic populations. It is thus a very difficult art to define because it spans some 1400 years, covering many lands and populations. This art is also not of a specific religion, time, place, or single medium. Instead Islamic art covers a range of artistic fields including architecture, calligraphy, painting, glass, ceramics, and textiles, among others. Islamic art is not restricted to religious art, but instead includes all of the art of the rich and varied cultures of Islamic societies. It frequently includes secular elements and elements that are forbidden by some Islamic theologians. Islamic religious art differs greatly from Christian religious art traditions. Because figural representations are generally considered to be forbidden in Islam, the word takes on religious meaning in art as seen in the tradition of calligraphic inscriptions. Calligraphy and the decoration of manuscript Qu’rans is an important aspect of Islamic art as the word takes on religious and artistic significance. Islamic art was influenced by Greek, Roman, early Christian, and Byzantine art styles, as well as the Sassanian art of pre-Islamic Persia. Central Asian styles were brought in with various nomadic incursions; and Chinese influences had a formative effect on Islamic painting, pottery, and textiles.",
   },
   {
     title: "Medieval Art",
-    info: `places painting are ..........`,
+    info: "At the beginning of the medieval period most significant works of art were very rare and costly objects associated with secular elites, monasteries or major churches and, if religius, largely made by monks. Subsequently, by the end of middle ages works considerable artistic interest could be found in the small town and significant number of bourgeois homes in towns, their production was in many places an important local industry.",
   },
   {
     title: "Modern Art",
@@ -333,7 +339,7 @@ const handleResponse = function (response) {
   return response.json();
 };
 
-const array = ["painting", "Places", "Tempera", "Egyptian Art"];
+const array = ["painting", "Places", "Tempera", "Egyptian Art", "Islamic Art"];
 
 // document on load
 const onReady = function () {
